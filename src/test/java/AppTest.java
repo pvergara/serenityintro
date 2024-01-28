df58e9f66632a7cede5c4c78dd1e0dc0ccd692f1
pvergara <ecos.logic.org@gmail.com>
2024-01-28 14:08:35 +0100

    Starting w/ tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,17 +1,35 @@
 import net.serenitybdd.annotations.Managed;
-import net.serenitybdd.junit.runners.SerenityRunner;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-@RunWith(SerenityRunner.class)
-public class AppTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SerenityJUnit5Extension.class)
+class AppTest extends PageObject
 {
     @Managed
     WebDriver driver;
 
     @Test
-    public void accessWikipedia(){
-        driver.get("https://en.wikipedia.com");
+    void accessWikipedia(){
+        openUrl("https://www.saucedemo.com/");
+
+        WebElement userNameSelector = driver.findElement(By.id("user-name"));
+        WebElement passwordSelector = driver.findElement(By.id("password"));
+        WebElement loginButtonSelector = driver.findElement(By.id("login-button"));
+
+        userNameSelector.sendKeys("performance_glitch_user");
+        passwordSelector.sendKeys("secret_sauce");
+        loginButtonSelector.click();
+
+        WebElement productSelector = driver.findElement(By.className("title"));
+
+
+        assertThat(productSelector.getText()).isEqualTo("Products");
     }
 }
