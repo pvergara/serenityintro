cdbe064b9be5b3ef47104234f35387b84a019f7f
Pablo Cristóbal Vergara Castro <pvergaca@ext.nttdata.com>
2024-02-07 10:01:46 +0100

    * Add new constants

---
+++ src/main/java/org/ecos/logic/serenity_intro/utils/Constant.java
@@ -0,0 +1,7 @@
+package org.ecos.logic.serenity_intro.utils;
+
+public class Constant {
+    public static final String PRODUCTS = "Products";
+    public static final String BACK_TO_PRODUCTS = "Back to products";
+
+}
