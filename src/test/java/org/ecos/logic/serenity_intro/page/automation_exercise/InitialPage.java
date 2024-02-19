f5a8cdbdd068b2df9354a3cd72fb3d089aca8b72
Pablo Cristóbal Vergara Castro <pvergaca@ext.nttdata.com>
2024-02-19 11:14:20 +0100

    Using lombok

--- src/test/java/org/ecos/logic/serenity_intro/page/automation_exercise/InitialPage.java
+++ src/test/java/org/ecos/logic/serenity_intro/page/automation_exercise/InitialPage.java
@@ -1,16 +1,16 @@
 package org.ecos.logic.serenity_intro.page.automation_exercise;
 
+import lombok.Getter;
 import net.serenitybdd.annotations.DefaultUrl;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 
+@Getter
 @DefaultUrl("https://automationexercise.com/")
 public class InitialPage extends PageObject {
     @FindBy(css = "a[href='/login']")
     private WebElementFacade signUp;
 
-    public WebElementFacade getSignUp() {
-        return signUp;
-    }
+
 }
